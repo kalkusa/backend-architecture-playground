5097c2588c25c83764f1cbcd6297fc162379ba28
kalkusa <kalkus@10g.pl>
2018-05-06 01:04:25 +0200

    Added simple web api.

---
+++ kalkusa.BackendPlayground.REST/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using kalkusa.BackendPlayground.REST.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace kalkusa.BackendPlayground.REST.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new User[] {
+                new User { Firstname = "Mark", Surname = "Zuckerberg", Id = new Guid() },
+                new User { Firstname = "Bill", Surname = "Gates", Id = new Guid() }
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return new User { Firstname = "Mark", Surname = "Zuckerberg", Id = new Guid() };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ kalkusa.BackendPlayground.REST/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace kalkusa.BackendPlayground.REST.Models
+{
+    public class User
+    {
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public Guid Id { get; set; }
+    }
+}
